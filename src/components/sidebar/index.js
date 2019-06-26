09e3d89c0833808a19dbf9e683c494924b045a1c
Caick <caick@teravoz.com.br>
2019-06-26 09:30:52 -0300

    Start of project, first commit

---
+++ src/components/sidebar/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+function Sidebar(prop) {
+  return (
+    <div className="sidebarWrapper">
+        <Link to="/"></Link>
+        <Link to="/"></Link>
+    </div>
+  )
+}
